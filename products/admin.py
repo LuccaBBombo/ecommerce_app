8698b2e99ec61c5b65ddd2a7ca15a58bcf86d7db
Lucca Bombo <lucca.bombo@hotmail.com>
2020-09-03 19:29:03 +0000

    added products app, fixtures and created products model

---
+++ products/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from .models import Product, Category
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'category',
+    )
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category)
