22a7a48c6000cb029cef320c09b1d7fbbb4487e7
Lucca Bombo <lucca.bombo@hotmail.com>
2020-09-09 12:56:41 +0000

    started creating products info page template

--- products/urls.py
+++ products/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.all_products, name='products'),
+    path('<product_id>', views.product_info, name='product_info'),
 ]
--- products/views.py
+++ products/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product
 
 
@@ -11,3 +11,14 @@ def all_products(request):
         'products': products,
     }
     return render(request, 'products/all_products.html', context)
+
+
+def product_info(request, product_id):
+    """A view that returns a detailed page of the selected product by targeting its ID"""
+
+    product = get_object_or_404(Product, pk=product_id)
+
+    context = {
+        'product': product,
+    }
+    return render(request, 'products/product_info.html', context)
