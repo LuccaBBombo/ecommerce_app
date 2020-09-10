5b74e38c8e3d921998b7d82c24216890a3c0ca8b
Lucca Bombo <lucca.bombo@hotmail.com>
2020-09-10 19:00:33 +0000

    started creating the sorting funcionality

--- products/views.py
+++ products/views.py
@@ -5,14 +5,34 @@ from .models import Product, Category
 
 
 def all_products(request):
-    """A view that returns a list of all the products, search and sort queries"""
+    """A view that returns a list of all the products,
+       search and sort queries"""
 
     products = Product.objects.all()
+    sort = None
+    direction = None
     query = None
     categories = None
 
-    """Query to filter objects by name, description or category"""
+    """
+    Query to filter objects by name, description or category
+    Also functionality to sort products
+    """
+
     if request.GET:
+        if 'sort' in request.GET:
+            sortkey = request.GET['sort']
+            sort = sortkey
+            if sortkey == 'name':
+                sortkey = 'lower_ name'
+                products = products.annotate(lower_name=Lower('name'))
+
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                if direction == 'desc':
+                    sortkey = f'-{sortkey}'
+            products = products.order_by(sortkey)
+
         if 'category' in request.GET:
             categories = request.GET['category'].split(',')
             products = products.filter(category__name__in=categories)
@@ -27,10 +47,13 @@ def all_products(request):
             queries = Q(name__icontains=query) | Q(description__icontains=query)
             products = products.filter(queries)
 
+    current_sorting = f'{sort}_{direction}'
+
     context = {
         'products': products,
         'search_term': query,
-        'selected_categories': categories,
+        'current_categories': categories,
+        'current_sorting': current_sorting,
     }
 
     return render(request, 'products/all_products.html', context)
