31a12013eb89b16811ca86c27b54ff6545841fd8
Lucca Bombo <lucca.bombo@hotmail.com>
2020-09-14 16:59:34 +0000

    linked cart app url to the overall url and made suere that it is working

--- cart/views.py
+++ cart/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def view_cart(request):
+    """A view to render the cart content page"""
+    return render(request, 'cart/cart.html')
