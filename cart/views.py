56de64c9a3f4280ffd6f3d2693d3c5d19a9674df
Lucca Bombo <lucca.bombo@hotmail.com>
2020-09-22 15:15:59 +0000

    created toasts with message when adding, updating and deletig items from cart

--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render, redirect, reverse, HttpResponse
+from django.contrib import messages
+
+from products.models import Product
 
 
 def view_cart(request):
@@ -9,6 +12,7 @@ def view_cart(request):
 def add_to_cart(request, item_id):
     """ Adds chosen quantity of a chosen item to the shopping cart"""
 
+    product = Product.objects.get(pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     cart = request.session.get('cart', {})
@@ -17,25 +21,25 @@ def add_to_cart(request, item_id):
         cart[item_id] += quantity
     else:
         cart[item_id] = quantity
+        messages.success(request, f'{product.name} was added to your cart')
 
     request.session['cart'] = cart
-
     return redirect(redirect_url)
 
 
 def update_cart(request, item_id):
     """ Updates item quantity in the cart for the selected item"""
-
+    product = Product.objects.get(pk=item_id)
     quantity = int(request.POST.get('quantity'))
     cart = request.session.get('cart', {})
 
     if quantity > 0:
         cart[item_id] = quantity
+        messages.success(request, f'{product.name} quantity has been updated to {cart[item_id]}')
     else:
         cart.pop[item_id]
 
     request.session['cart'] = cart
-
     return redirect(reverse('view_cart'))
 
 
@@ -43,12 +47,12 @@ def remove_item_cart(request, item_id):
     """ Deletes the selected item in the cart"""
 
     try:
+        product = Product.objects.get(pk=item_id)
         cart = request.session.get('cart', {})
-
         cart.pop(item_id)
+        messages.success(request, f'{product.name} was removed from your cart')
 
         request.session['cart'] = cart
-
         return HttpResponse(status=200)
     except Exception:
         return HttpResponse(status=500)
