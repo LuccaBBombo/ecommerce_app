3ca0f74f3514bd0ad2e3b47b5d445dc6e35443d8
Lucca Bombo <lucca.bombo@hotmail.com>
2020-09-26 08:19:40 +0000

    added payment intent key to the handle paymeny succeeded webhook to store the chackout_cache data

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -47,22 +47,34 @@ form.addEventListener('submit', function(ev) {
     $('#submit-form-button').attr('disabled', true);
     $('#pay-form').fadeToggle(100); // Toggles the loading overlay when complete payment button is activated
     $('#loading-payment-overlay').fadeToggle(100);
-    stripe.confirmCardPayment(clientSecret, {
-        payment_method: {
-            card: card,
-            billing_details: {
-                    name: $.trim(form.full_name.value),
-                    phone: $.trim(form.phone_number.value),
-                    email: $.trim(form.email.value),
-                    address:{
-                        line1: $.trim(form.street_address1.value),
-                        line2: $.trim(form.street_address2.value),
-                        city: $.trim(form.town_or_city.value),
-                        country: $.trim(form.country.value),
-                        state: $.trim(form.county.value),
+
+     var saveInfo = Boolean($('#id-save-delivery-info').attr('checked'));
+    // From using {% csrf_token %} in the checkout form
+    var csrfToken = $('input[name="csrfmiddlewaretoken"]').val();
+    var postData = {
+        'csrfmiddlewaretoken': csrfToken,
+        'client_secret': clientSecret,
+        'id-save-delivery-info': saveInfo,
+    };
+    var url = '/checkout/cache_checkout_data/';
+
+    $.post(url, postData).done(function() {
+        stripe.confirmCardPayment(clientSecret, {
+            payment_method: {
+                card: card,
+                billing_details: {
+                        name: $.trim(form.full_name.value),
+                        phone: $.trim(form.phone_number.value),
+                        email: $.trim(form.email.value),
+                        address:{
+                            line1: $.trim(form.street_address1.value),
+                            line2: $.trim(form.street_address2.value),
+                            city: $.trim(form.town_or_city.value),
+                            country: $.trim(form.country.value),
+                            state: $.trim(form.county.value),
+                        }
                     }
-                }
-            },
+                },
             shipping: {
                 name: $.trim(form.full_name.value),
                 phone: $.trim(form.phone_number.value),
@@ -75,23 +87,27 @@ form.addEventListener('submit', function(ev) {
                     state: $.trim(form.county.value),
                 }
         }
-    }).then(function(result) {
-        if (result.error) {
-            var error = document.getElementById('card-errors');
-            var html = `
-                <span class="icon" role="alert">
-                <i class="fas fa-times"></i>
-                </span>
-                <span>${result.error.message}</span>`;
-            $(error).html(html);
-            $('#pay-form').fadeToggle(100);
-            $('#loading-payment-overlay').fadeToggle(100);
-            card.update({ 'disabled': false});
-            $('#submit-button').attr('disabled', false);
-        } else {
-            if (result.paymentIntent.status === 'succeeded') {
-                form.submit();
+        }).then(function(result) {
+            if (result.error) {
+                var error = document.getElementById('card-errors');
+                var html = `
+                    <span class="icon" role="alert">
+                    <i class="fas fa-times"></i>
+                    </span>
+                    <span>${result.error.message}</span>`;
+                $(error).html(html);
+                $('#pay-form').fadeToggle(100);
+                $('#loading-payment-overlay').fadeToggle(100);
+                card.update({ 'disabled': false});
+                $('#submit-button').attr('disabled', false);
+            } else {
+                if (result.paymentIntent.status === 'succeeded') {
+                    form.submit();
+                }
             }
-        }
-    });
+        });
+    }).fail(function () {
+        // just reload the page and displays the error in django messages
+        location.reload();
+    })
 });
--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -17,6 +17,8 @@ class StripeWH_Handler:
 
     def handle_payment_intent_succeeded(self, event):
         """Handle the payment_intent.succeeded webhook from Stripe"""
+        intent = event.data.object #Store payment intent 
+        print(intent)
         return HttpResponse(
             content=f'Webhook received: {event["type"]}',
             status=200)
