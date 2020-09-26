5949ea0bf00ed336fa4f87d0281c59a4acace889
Lucca Bombo <lucca.bombo@hotmail.com>
2020-09-26 09:52:23 +0000

    added view item button and completed webhooks

--- checkout/admin.py
+++ checkout/admin.py
@@ -11,10 +11,10 @@ class OrderLineItemAdminInline(admin.TabularInline):
 class OrderAdmin(admin.ModelAdmin):
     inlines = (OrderLineItemAdminInline,)
     readonly_fields = ('order_number', 'date', 'delivery_cost',
-                       'order_total', 'grand_total',)
+                       'order_total', 'grand_total', 'original_cart', 'stripe_pid')
     fields = ('order_number', 'full_name', 'email', 'phone_number',
               'country', 'postcode', 'town_or_city', 'street_address1', 'street_address2',
-              'county', 'date', 'delivery_cost', 'order_total', 'grand_total',)
+              'county', 'date', 'delivery_cost', 'order_total', 'grand_total', 'original_cart', 'stripe_pid')
 
     list_display_fields = ('order_number', 'full_name',
                            'email', 'order_total', 'delivery_cost', 'grand_total',)
--- checkout/views.py
+++ checkout/views.py
@@ -20,7 +20,7 @@ def cache_checkout_data(request):
         stripe.api_key = settings.STRIPE_SECRET_KEY
         stripe.PaymentIntent.modify(pid, metadata={
             'cart': json.dumps(request.session.get('cart', {})),
-            'save_info': request.POST.get('save_info'),
+            'save_info': request.POST.get('id-save-delivery-info'),
             'username': request.user,
         })
         return HttpResponse(status=200)
@@ -50,7 +50,11 @@ def checkout(request):
         }
         order_form = OrderForm(form_data)
         if order_form.is_valid():
-            order = order_form.save()
+            order = order_form.save(commit=False)
+            pid = request.POST.get('client_secret').split('_secret')[0]
+            order.stripe_pid = pid
+            order.original_cart = json.dumps(cart)
+            order.save()
             for item_id, quantity in cart.items():
                 try:
                     product = Product.objects.get(id=item_id)
