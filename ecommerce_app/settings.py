a80d9920b8142fda8d3f20828614bbc8ea0009a7
Lucca Bombo <lucca.bombo@hotmail.com>
2020-09-28 19:53:35 +0000

    added Secret key environ and changed debug to true only if there is a variable called development in the environment

--- ecommerce_app/settings.py
+++ ecommerce_app/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'ipko(o0v*z1rfu0q9o1jy=wv#6hqek56s)&!nfvcrx+#)zquce'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['eletrohome.herokuapp.com', 'localhost']
 
