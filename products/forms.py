2e3ca09389a97d9b044d8d01e41d8ecb6456b558
Lucca Bombo <lucca.bombo@hotmail.com>
2020-09-28 16:33:50 +0000

    added home icon link to mobile header and customized the image iput field on the add and edit products pages

--- products/forms.py
+++ products/forms.py
@@ -1,11 +1,14 @@
 from django import forms
 from .models import Product, Category
+from .widgets import CustomClearableFileInput
 
 
 class ProductForm(forms.ModelForm):
     class Meta:
         model = Product
         fields = '__all__'
+
+    image = forms.ImageField(label='image', required=True, widget=CustomClearableFileInput)
 
     def ___ini__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
