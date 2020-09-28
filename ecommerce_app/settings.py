365dea5171dc7c45d51f1e250e9177bb9e1add5b
Lucca Bombo <lucca.bombo@hotmail.com>
2020-09-28 19:43:49 +0000

    continue setting up app for deployment on heroku

--- ecommerce_app/settings.py
+++ ecommerce_app/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'ipko(o0v*z1rfu0q9o1jy=wv#6hqek56s)&!nfvcrx+#)zquce'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['eletrohome.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -118,12 +118,17 @@ WSGI_APPLICATION = 'ecommerce_app.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-         'NAME': BASE_DIR / 'db.sqlite3',
-     }
- }
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 
