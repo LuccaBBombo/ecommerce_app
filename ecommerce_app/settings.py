7dce4aed15266c45736e1e943b9db5197e0fce63
Lucca Bombo <lucca.bombo@hotmail.com>
2020-09-29 16:32:43 +0000

    changed the s3 region name

--- ecommerce_app/settings.py
+++ ecommerce_app/settings.py
@@ -178,7 +178,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 if 'USE_AWS' in os.environ:
     # Bucket Configuration for Heroku
     AWS_STORAGE_BUCKET_NAME = 'eletrohomr'
-    AWS_S3_REGION_NAME = 'eu-ireland'
+    AWS_S3_REGION_NAME = 'eu-west-1'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
