fb0d172f6b117f7181c33cfb9d0c0f72cce3ccc3
Lucca Bombo <lucca.bombo@hotmail.com>
2020-09-29 16:27:34 +0000

    Added configuration to AWS Storage to access media and static files and updated requirements.txt file

--- ecommerce_app/settings.py
+++ ecommerce_app/settings.py
@@ -48,7 +48,8 @@ INSTALLED_APPS = [
     'profiles',
     'checkout',
     # Other
-    'crispy_forms'  # Package from django used to allow forms to be customized by bootstrap
+    'crispy_forms',  # Package from django used to allow forms to be customized by bootstrap
+    'storages',
 ]
 
 MIDDLEWARE = [
@@ -173,6 +174,24 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+if 'USE_AWS' in os.environ:
+    # Bucket Configuration for Heroku
+    AWS_STORAGE_BUCKET_NAME = 'eletrohomr'
+    AWS_S3_REGION_NAME = 'eu-ireland'
+    AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+
+    # Static and media files
+    STATICFILES_STORAGE = 'custom_storages.StaticStorage'
+    STATICFILES_LOCATION = 'static'
+    DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
+    MEDIAFILES_LOCATION = 'media'
+
+    # Override static and media URLs in production
+    STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
+    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
 
 # Stripe Settings
 STANDARD_DELIVERY_PERCENTAGE = 5
